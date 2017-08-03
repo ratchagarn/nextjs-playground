49133d268d851388eb1239ff5f53537093c2c315
Ratchagarn Naewbuntad <ratchagarn@omise.co>
2017-08-03 18:35:25 +0700

    Add server for support custom/clean URL.

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,29 @@
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
  */
 import Layout from '../components/Layout';
+import Link from 'next/link';
+
 
 export default function Index() {
   return (
     <Layout>
-      <p>Hello next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" title="Hello Next.js" />
+        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   );
 };
+
+
+function PostLink(props) {
+  return (
+    <li>
+      <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+        <a>{props.title}</a>
+      </Link>
+    </li>
+  );
+}
